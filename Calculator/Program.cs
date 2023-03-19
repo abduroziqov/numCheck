5723b8c2c21097b9322a0d9f4fa0bbfaa8180f65
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-19 16:35:21 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int result = 0;
+
+            Console.Write("Enter operation type for two numbers ( +, -, *, / ) => ");
+            ConsoleKey keys = Console.ReadKey().Key;
+            Console.WriteLine();
+
+            Console.Write("Enter first value => "); int.TryParse(Console.ReadLine(), out int firstNum);
+            Console.Write("Enter second value => "); int.TryParse(Console.ReadLine(), out int secondNum);
+
+            switch (keys)
+            {
+                case ConsoleKey.Add: result = firstNum + secondNum; break;
+                case ConsoleKey.Subtract: result = firstNum - secondNum; break;
+                case ConsoleKey.Multiply: result = firstNum * secondNum; break;
+                case ConsoleKey.Divide: if (secondNum != 0) result = firstNum / secondNum; break;
+            }
+
+            Console.WriteLine($"Result is {result}");
+
+            Main();
+        }
+    }
+}
---
+++ is_prime_check/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace is_prime_check
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number => ");
+            int.TryParse(Console.ReadLine(), out int firstNum);
+
+            int count = 0;
+
+            for (int i = 1; i < firstNum; i++) if (firstNum % i == 0) count++;
+
+            if (count <= 2) Console.WriteLine("The number is prime: " + firstNum);
+            else Console.WriteLine("The number is not prime: " + firstNum);
+
+            Main();
+        }
+    }
+}
